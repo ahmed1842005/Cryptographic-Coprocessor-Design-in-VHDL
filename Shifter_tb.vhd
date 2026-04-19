135f3dfe7bbe2b9d44be9f15d83d1c94eea071ae
Ahmed Waleed Elgebali <ahmedelgebali574@gmail.com>
2026-04-19 21:42:15 +0200

    Modify Shifter testbench for control signal changes
    Updated control signal width and modified test cases for the Shifter component.

--- Shifter_tb.vhd
+++ Shifter_tb.vhd
@@ -7,23 +7,20 @@ end entity;
 
 architecture sim of Shifter_TB is
 
-    
     component Shifter
         port(
             input      : in  std_logic_vector(15 downto 0);
-            input_CTRL : in  std_logic_vector(3 downto 0);
+            input_CTRL : in  std_logic_vector(1 downto 0);
             output     : out std_logic_vector(15 downto 0)
         );
     end component;
 
-    
-    signal flag_input      : std_logic_vector(15 downto 0);
-    signal flag_CTRL       : std_logic_vector(3 downto 0);
-    signal flag_output     : std_logic_vector(15 downto 0);
+    signal flag_input  : std_logic_vector(15 downto 0);
+    signal flag_CTRL   : std_logic_vector(1 downto 0);
+    signal flag_output : std_logic_vector(15 downto 0);
 
 begin
 
-    
     DUT: Shifter
         port map(
             input      => flag_input,
@@ -31,36 +28,39 @@ begin
             output     => flag_output
         );
 
-   
     process
     begin
         
-       
+        -- Test 1: Pass
         flag_input <= x"00FF";
-        flag_CTRL  <= "1000";
+        flag_CTRL  <= "00";
         wait for 20 ns;
 
-       
-        flag_CTRL <= "1001";
+        -- Test 2: Rotate Right 8
+        flag_CTRL <= "01";
         wait for 20 ns;
 
-        
-        flag_CTRL <= "1010";
+        -- Test 3: Rotate Right 4
+        flag_CTRL <= "10";
         wait for 20 ns;       
 
-        
-        flag_CTRL <= "0000";
+        -- Test 4: Shift Left 8
+        flag_CTRL <= "11";
         wait for 20 ns;
-            
 
+        -- Test 5: New Input
         flag_input <= x"F0F0";
-        flag_CTRL  <= "1000";
+
+        flag_CTRL <= "00";
         wait for 20 ns;
 
-        flag_CTRL <= "1001";
+        flag_CTRL <= "01";
         wait for 20 ns;
 
-        flag_CTRL <= "1010";
+        flag_CTRL <= "10";
+        wait for 20 ns;
+
+        flag_CTRL <= "11";
         wait for 20 ns;
 
         wait;
