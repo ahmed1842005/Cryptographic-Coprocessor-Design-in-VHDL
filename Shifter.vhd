42e13e925809cb156a8d3d12704a54b6056289d3
Ahmed Waleed Elgebali <ahmedelgebali574@gmail.com>
2026-04-19 21:41:20 +0200

    Modify input_CTRL size and update control cases

--- Shifter.vhd
+++ Shifter.vhd
@@ -5,7 +5,7 @@ use ieee.numeric_std.all;
 entity Shifter is 
 	port(
 		input   : in  std_logic_vector(15 downto 0);
-		input_CTRL : in  std_logic_vector(3 downto 0);
+		input_CTRL : in  std_logic_vector(1 downto 0);
 		output  : out std_logic_vector(15 downto 0) 
 	);										    
 end entity;
@@ -17,17 +17,20 @@ begin
 	begin
 		case input_CTRL is
 			
-			when "1000" =>                       -- Rotate_Right_8
+			when "00" =>  -- Pass Through
+				output <= input;
+
+			when "01" =>  -- Rotate Right 8
 				output <= std_logic_vector(
 					rotate_right(unsigned(input), 8)
 				);
 
-			when "1001" =>                       -- Rotate_Right_4
+			when "10" =>  -- Rotate Right 4
 				output <= std_logic_vector(
 					rotate_right(unsigned(input), 4)
 				);
 
-			when "1010" =>                       -- Shift_left_8
+			when "11" =>  -- Shift Left 8
 				output <= std_logic_vector(
 					shift_left(unsigned(input), 8)
 				);
